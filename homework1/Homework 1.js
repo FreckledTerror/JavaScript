7d95d9743af397ff5da70a4a652f36a70f35f511
Hunter Lopez <FreckledTerror97Programming@gmail.com>
2020-01-23 17:59:44 -0500

    Tweaked the Homework 1 file to fix the issue of it not working

--- homework1/Homework 1.js
+++ homework1/Homework 1.js
@@ -2,4 +2,4 @@ let myName = "Hunter Lopez";
 let myMajor = "General CIS";
 let myFact = "I'm a Mexican Ginger";
 
-console.log(myName, "/n", myMajor, "/n", myFact);
+console.log(myName + "\n" + myMajor + "\n" + myFact);
