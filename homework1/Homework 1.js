322648094b196049e63a15f2c423db94b9e95cad
Hunter Lopez <FreckledTerror97Programming@gmail.com>
2020-01-23 18:44:09 -0500

    Made Homework 1.html a basic HTML file with it sourcing the JS file, and then put in all the things needed for the homework in the JS file, but the loops aren't working, gotta work on that

--- homework1/Homework 1.js
+++ homework1/Homework 1.js
@@ -1,5 +1,35 @@
+const CLASS = "CIS-172";
+let emailAddress = "FreckledTerror97Programming@gmail.com";
+
 let myName = "Hunter Lopez";
 let myMajor = "General CIS";
 let myFact = "I'm a Mexican Ginger";
+let loop = 0;
 
-console.log(myName + "\n" + myMajor + "\n" + myFact);
+//Tested the basic facts to see if they worked
+//console.log(myName + "\n" + myMajor + "\n" + myFact);
+
+console.log(emailAddress);
+
+console.log("I am using JS in " + CLASS);
+
+for(let i = 0; i++; i >= 10)
+{
+    console.log("For loop: " + i)
+}
+
+while(loop >= 10)
+{
+    console.log("While loop: " + loop);
+    loop++;
+}
+
+if(myName === "Mr. Banks")
+{
+    console.log("Welcome back Professor!");
+}
+else
+{
+    console.log("Welcome " + myName + " to " + CLASS + ". \nYou are majoring in: "
+    + myMajor);
+}
