3d8df9eb72fe36e21476576883447e43c460152f
Hunter Lopez <FreckledTerror97Programming@gmail.com>
2020-01-30 18:12:18 -0500

    Put in the Questions for Quiz 1 and made Quiz 1.html source to Quiz 1.js for testing and did all the work in Quiz 1.js

---
+++ Quizes/Quiz 1/Quiz 1.js
@@ -0,0 +1,60 @@
+/**
+ * Quiz 1 focuses on variables and control structures
+ *
+ * @category    Quiz 1
+ * @author      Hunter Lopez <hwlopez@hawkmail.hfcc.edu>
+ * @date        2020.01.30
+ * @link
+    */
+
+//Question 1
+const favBook = "Dragon Rider";
+
+let favInt = 42;
+
+if(favInt > 42)
+{
+    console.log("BIGGER");
+}
+else if(favInt === 42)
+{
+    console.log("EQUAL");
+}
+else
+{
+    console.log("SMALLER");
+}
+
+for(let i = 0; i < 7; i++)
+{
+    console.log(favBook);
+}
+
+while(favInt >= -42)
+{
+    if(favInt === 0)
+    {
+        console.log("Zero detected!")
+    }
+
+    favInt--;
+}
+
+switch(favBook)
+{
+    case "The Matrix":
+        console.log("Excellent Movie!");
+        break;
+    case "Tears of Steel":
+        console.log("I love open source!");
+        break;
+    case "Fight Club":
+        console.log("Excellent Movie!");
+        break;
+    case "Citizen Kane":
+        console.log("All Time Best Movie!");
+        break;
+    case "Star Wars":
+        console.log("He didn't say Luke I am your father!");
+        break;
+}
