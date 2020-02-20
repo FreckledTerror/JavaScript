f9d082a50f09172bf42f1109712d42de522deb5b
Hunter Lopez <FreckledTerror97Programming@gmail.com>
2020-02-20 18:06:52 -0500

    Did Quiz 3

---
+++ Quizes/Quiz 3/Quiz 3.js
@@ -0,0 +1,33 @@
+/**
+ * Quiz 3 focuses on Objects
+ *
+ * @category    Quiz 3
+ * @author      Chad Banks <crbanks1@hfcc.edu>
+ * @author      Hunter Lopez <hwlopez@hawkmail.hfcc.edu>
+ * @date        2020.02.20
+ * @link
+    */
+let SiteUser = {
+    username: "hwlopez",
+    email: "hwlopez@hawkmail.hfcc.edu",
+    password: "password1234", //Passwords are fun
+    cart:[],
+    cell: "555-867-5309",
+    visits: 5,
+    preferences: "preferences go here",
+    lastSignIn: "20200130", //Military date for Jan 30th, 2020
+
+    addVisit: function ()
+    {
+        visits += 1;
+    },
+
+    addItemToCart: function (item)
+    {
+        cart.push(item);
+    }
+};
+
+SiteUser.addVisit();
+
+SiteUser.addItemToCart("$2000 gaming machine"); //Figured I should call it to show that it works
