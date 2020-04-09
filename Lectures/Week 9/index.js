5bef1404da86d07b23674a536cfc1137b6251f83
Hunter Lopez <hwlopez@hawkmail.hfcc.edu>
2020-04-09 16:51:42 -0400

    Grabbed the lecture notes on Weeks 9 and 10

--- Lectures/Week 9/index.js
+++ Lectures/Week 9/index.js
@@ -1,23 +1,89 @@
+/**
+ * Week 9 - Validating Form Data
+ *
+ * @category    Week 9
+ * @author      Chad Banks <crbanks1@hfcc.edu>
+ * @date        2020.03.31
+ **/
+
 let contactFormValidator = {
-    init: function ()
-    {
+
+    contactForm : null,
+    isValid : false,
+
+    init: function (){
         console.log('Page loaded!');
 
         let contactName = document.getElementById('contactName');
-        contactName.addEventListener("blur", contactFormValidator.validateName);
+        contactName.addEventListener( "blur", contactFormValidator.validateName );
+
+        contactFormValidator.contactForm = document.getElementById('contactForm');
+        contactFormValidator.contactForm.addEventListener( "submit", contactFormValidator.validateForm );
+        console.log("Form:", contactFormValidator.contactForm);
     },
-
-    validateName: function (event)
+    validateName : function( event )
     {
-        console.log("Event: " + event);
-        console.log("Value Length:", event.target.value.length);
+        let nameInput = document.getElementById('contactName');
 
-        if(event.target.value.length < 3)
+        //console.log("Event:", event);
+        //console.log("Value Length:", event.target.value.length);
+
+        if( typeof nameInput.value !== "string" )
         {
-            // Fire error here
-            //Example: console.error
+            alert('Name should be a string!');
+            return false;
+        }
+
+        // dateInput.value
+        // let currentDate = new Date(  );
+        // let usersDate = new Date( dateInput.value );
+        // currentDate.getMonth() <= 11
+
+        if(nameInput.value.length < 3)
+        {
+            // Fire error message here
+            // Example: console.error
+            // Example: Try/Catch -> throw new error('validation')
+            // Example: alert('validation error!');
+            // Example: Custom modal or pop-up (preferred)
+            alert('Name should be longer than 2 characters!');
+
+            return false;
+        }
+
+        let spaceIndex = nameInput.value.indexOf(" ");
+        //let s3Index = nameInput.value.indexOf("   ");
+
+        if( spaceIndex === -1 )
+        {
+            alert('No space error, name should be a full name!');
+
+            return false;
+        }
+
+        return true;
+
+    },
+    validateForm : function(event)
+    {
+        event.preventDefault();
+
+        // Do ALL validation
+        let isValid = true;
+
+        if( !contactFormValidator.validateName() )
+        {
+            isValid = false;
+        }
+
+        // New validations
+
+        if( isValid )
+        {
+            contactFormValidator.contactForm.submit();
         }
     }
 };
+
 
 window.onload = contactFormValidator.init;
---
+++ Lectures/Week 9/lab.js
@@ -0,0 +1,60 @@
+/**
+ * Week 9 - Homework Lab
+ *
+ * @category    Week 9
+ * @author      Chad Banks <crbanks1@hfcc.edu>
+ * @date        2020.04.02
+ **/
+
+
+let myObject = {
+
+    debugButton : null,
+
+    init : function()
+    {
+        myObject.debugButton = document.getElementById('debugButton');
+        myObject.debugButton.addEventListener("click", myObject.checkDate );
+        console.log( "button:", myObject.debugButton );
+    },
+    checkDate : function() {
+        try {
+            console.log("Clicked!");
+
+            // Done some other stuff
+
+            let errorMessage = '<span style="color:red;">Sorry, an error occurred!</span>';
+
+            let myDate = new Date();
+            console.log('myDate:', myDate);
+            console.log('Month:', myDate.getMonth());
+        }
+        catch(e)
+        {
+            console.error(e);
+        }
+    },
+    func2 : function() {
+        try {
+            // Code....
+        }
+        catch(e)
+        {
+            console.error(e);
+        }
+    },
+    func3 : function()
+    {
+        try {
+            // Code...
+        }
+        catch(e)
+        {
+            console.error(e);
+        }
+    }
+
+};
+
+
+window.onload = myObject.init;
