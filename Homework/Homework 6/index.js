1bb4d83abbc83e6ca1a4d6544207f7440fc14de8
Hunter Lopez <hwlopez@hawkmail.hfcc.edu>
2020-04-16 18:21:02 -0400

    Finished Quiz 7 and Homework 6, stuck on Homework 6, but I don't have time to fix it, so I'm just taking what I get

--- Homework/Homework 6/index.js
+++ Homework/Homework 6/index.js
@@ -8,28 +8,51 @@
 
 let PageHandler =
 {
-    pageDiv,
-
 
     init: function ()
     {
         try
         {
+
+            PageHandler.pageDiv = document.getElementById("JohnCena");
+            PageHandler.btnDiv = document.getElementById("btn1");
+            PageHandler.btnDoc = document.getElementById("btn2");
+
             console.log("Page has loaded");
 
-            if(!window.jQuery)
+            if(window.jQuery)
+            {
+                console.log("jQuery has loaded");
+            }
+            else
             {
                 throw Error;
             }
 
-            PageHandler.pageDiv.hiddenDiv = $('#JohnCena');
+            PageHandler.finalize();
 
         }
         catch(error)
         {
-            console.log("jQuery didn't load!");
+            console.log(error);
         }
+    },
+
+    finalize: function ()
+    {
+        PageHandler.pageDiv.hide = $('#JohnCena');
+
+        PageHandler.btnDiv.bind = $('btn1');
+
+        //This is where I'm stuck, from here on out I can't get it to work, it's stuck on this, and I don't have the
+        // time to fix it, thought it'd be easier to fix
+        PageHandler.btnDiv.click(PageHandler.pageDiv.show());
+
+        PageHandler.btnDoc.bind = $('btn2');
+
+        PageHandler.click('<a href link="https://api.jquery.com/></a>');
     }
+
 };
 
 window.onload = PageHandler.init;
